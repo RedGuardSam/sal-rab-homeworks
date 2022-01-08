5ee423e0e0a50252ad2f20ebeccbe847f15908ff
Vladimir Shekunov <vlshekunov@gmail.com>
2022-01-08 20:28:02 +0500

    Task 2 done v1.3

--- task_2/solution.js
+++ task_2/solution.js
@@ -8,10 +8,11 @@ function calcShipping(sum, min, shipping) {
     let shippingSum;       // создайте переменную shippingSum
     if(productsSum == 0 || productsSum >= freeShippingMinSum){  // если productsSum равно 0,
     shippingSum = 0;       // то shippingSum присвоить значение 0
-    }                       // если productsSum Больше или равна freeShippingMinSum,
+                         // если productsSum Больше или равна freeShippingMinSum,
                            // то shippingSum присвоить значение 0
-    if(productsSum > 0 && productsSum < freeShippingMinSum){    // если productsSum больше 0 и меньше freeShippingMinSum,
-    shippingSum = shipping;                                     // то shippingSum присвоить значение shippingPrice
+    }else{
+    (productsSum > 0 && productsSum < freeShippingMinSum)    // если productsSum больше 0 и меньше freeShippingMinSum,
+    shippingSum = shipping;                                      // то shippingSum присвоить значение shippingPrice
     }
     // Конец решения задания №2.1.
 
